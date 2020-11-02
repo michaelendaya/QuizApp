b455a12d173676a0ba8b92e6aabccf1d2945375e
Falqueza Miguel <falquezamiguel@gmail.com>
2020-11-02 11:42:17 +0800

    list to details fragment navigation, list to details fragment animation (and back), progress widget in list fragment, jetpack safe-args plugin for passing data

--- app/src/main/java/com/tvacstudio/quizzapp/ListFragment.java
+++ app/src/main/java/com/tvacstudio/quizzapp/ListFragment.java
@@ -8,22 +8,33 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
 
 import java.util.List;
 
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment implements QuizListAdapter.OnQuizListItemClicked {
+
+    private NavController navController;
 
     private RecyclerView listView;
     private QuizListViewModel quizListViewModel;
 
     private QuizListAdapter adapter;
+    private ProgressBar listProgress;
+
+    private Animation fadeInAnim;
+    private Animation fadeOutAnim;
 
     public ListFragment() {
         // Required empty public constructor
@@ -40,13 +51,18 @@ public class ListFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        navController = Navigation.findNavController(view);
+
         listView = view.findViewById(R.id.list_view);
-        adapter = new QuizListAdapter();
+        listProgress = view.findViewById(R.id.list_progress);
+        adapter = new QuizListAdapter(this);
 
         listView.setLayoutManager(new LinearLayoutManager(getContext()));
         listView.setHasFixedSize(true);
         listView.setAdapter(adapter);
 
+        fadeInAnim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
+        fadeOutAnim = AnimationUtils.loadAnimation(getContext(), R.anim.fade_out);
     }
 
     @Override
@@ -57,9 +73,19 @@ public class ListFragment extends Fragment {
         quizListViewModel.getQuizListModelData().observe(getViewLifecycleOwner(), new Observer<List<QuizListModel>>() {
             @Override
             public void onChanged(List<QuizListModel> quizListModels) {
+                //Load RecyclerView
+                listView.startAnimation(fadeInAnim);
+                listProgress.startAnimation(fadeOutAnim);
                 adapter.setQuizListModels(quizListModels);
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        ListFragmentDirections.ActionListFragmentToDetailsFragment action = ListFragmentDirections.actionListFragmentToDetailsFragment();
+        action.setPosition(position);
+        navController.navigate(action);
     }
 }
--- app/src/main/java/com/tvacstudio/quizzapp/QuizListAdapter.java
+++ app/src/main/java/com/tvacstudio/quizzapp/QuizListAdapter.java
@@ -17,6 +17,11 @@ import java.util.List;
 public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.QuizViewHolder> {
 
     private List<QuizListModel> quizListModels;
+    private OnQuizListItemClicked onQuizListItemClicked;
+
+    public QuizListAdapter(OnQuizListItemClicked onQuizListItemClicked){
+    this.onQuizListItemClicked = onQuizListItemClicked;
+    }
 
     public void setQuizListModels(List<QuizListModel> quizListModels) {
         this.quizListModels = quizListModels;
@@ -59,7 +64,7 @@ public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.QuizVi
         }
     }
 
-    public class QuizViewHolder extends RecyclerView.ViewHolder {
+    public class QuizViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private ImageView listImage;
         private TextView listTitle;
         private TextView listDesc;
@@ -74,6 +79,17 @@ public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.QuizVi
             listDesc = itemView.findViewById(R.id.list_desc);
             listLevel = itemView.findViewById(R.id.list_difficulty);
             listBtn = itemView.findViewById(R.id.list_btn);
+
+            listBtn.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+        onQuizListItemClicked.onItemClicked(getAdapterPosition());
+        }
+    }
+
+    public interface OnQuizListItemClicked {
+        void onItemClicked(int position);
     }
 }
